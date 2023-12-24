3003714961fdab6f7b644c0c35c70770367c9c5a
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-12-24 19:09:37 +0545

    Added & styled content on about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,28 @@
 import React from "react"
 import Wrapper from "../sections/Wrapper"
+import avatarImage from "../assets/sabin.jpg"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 function About() {
-  return <div>About</div>
+  return (
+    <div className="profile">
+      <img src={avatarImage} alt="avatar" className="profile-image" />
+      <h1 className="profile-text">Hi I am Sabin Shrestha</h1>
+      <h2 className="profile-text">The creator of this awesome pokepedia</h2>
+      <h4 className="profile-text">
+        This project is created for learning purpose only.
+      </h4>
+
+      <div className="profile-links">
+        <a href="https://github.com/sabinshrestha8" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/sabinshrestha8" target="_blank">
+          <FaLinkedin />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Wrapper(About)

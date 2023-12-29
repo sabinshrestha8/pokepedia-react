74da6e9c59305239abb3c2fcc0c91499227bc036
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-12-29 13:47:34 +0545

    Added getRecursiveEvolution(), getPokemonInfo() fns to get pokemonInfo & pokemon evolution data recursively & getcolor() fn to extract most dominant color as accent-color & set it to root element

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,2 +1,12 @@
 export const pokemonAPI = "https://pokeapi.co/api/v2"
 export const pokemonsRoute = `${pokemonAPI}/pokemon?limit=5000`
+export const pokemonRoute = `${pokemonAPI}/pokemon`
+export const pokemonSpeciesRoute = `${pokemonAPI}/pokemon-species`
+
+// Tabbing Structure
+export const pokemonTabs = {
+  description: "description",
+  evolution: "evolution",
+  locations: "locations",
+  moves: "moves",
+}
